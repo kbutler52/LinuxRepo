10dc79c3c4cd3866416edd61d7872fe72e73a680
Kyla Butler <kbutler30@bellsouth.net>
2019-12-24 22:31:57 +0000

    list/loop

---
+++ list_add.py
@@ -0,0 +1,7 @@
+list_add =[]
+def listadd (dog,pig):
+    dog.append (pig)
+
+listadd (list_add,"pig")
+
+print (list_add)
---
+++ loop.py
@@ -0,0 +1,11 @@
+count =1
+while count<=7:
+    if count % 2==0:
+        print (count)
+    count+=1
+
+
+for count in range(1,8):
+    if count % 2==0:
+        print(count)
+
